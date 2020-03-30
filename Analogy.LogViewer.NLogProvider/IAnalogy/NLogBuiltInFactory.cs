245328b3c4363ea880f70fc12865982f38c76314
Lior Banai <liorbanai@gmail.com>
2020-03-30 22:56:00 +0300

    update interface

--- Analogy.LogViewer.NLogProvider/IAnalogy/NLogBuiltInFactory.cs
+++ Analogy.LogViewer.NLogProvider/IAnalogy/NLogBuiltInFactory.cs
@@ -14,46 +14,25 @@ namespace Analogy.LogViewer.NLogProvider
     public class NLogBuiltInFactory : IAnalogyFactory
     {
         public static Guid AnalogyNLogGuid { get; } = new Guid("33CBFA00-DA3E-4F9F-B5A1-BE978FD09D57");
-        public Guid FactoryID { get; } = AnalogyNLogGuid;
+        public Guid FactoryId { get; } = AnalogyNLogGuid;
         public string Title { get; } = "Analogy NLogs Parser";
-        public IAnalogyDataProvidersFactory DataProviders { get; }
-        public IAnalogyCustomActionsFactory Actions { get; }
         public IEnumerable<IAnalogyChangeLog> ChangeLog => NLogProvider.ChangeLog.GetChangeLog();
         public IEnumerable<string> Contributors { get; } = new List<string> { "Lior Banai" };
         public string About { get; } = "Analogy NLogs Parser";
-        public NLogBuiltInFactory()
-        {
-            DataProviders = new AnalogyNLogDataProviderFactory();
-            Actions = new AnalogyNLogCustomActionFactory();
-
-        }
-
-
     }
 
     public class AnalogyNLogDataProviderFactory : IAnalogyDataProvidersFactory
     {
+        public Guid FactoryId { get; } = NLogBuiltInFactory.AnalogyNLogGuid;
         public string Title { get; } = "Analogy NLogs Data Provider";
-        public IEnumerable<IAnalogyDataProvider> Items { get; }
-
-        public AnalogyNLogDataProviderFactory()
-        {
-            var builtInItems = new List<IAnalogyDataProvider>();
-            var adpnlog = new NLogDataProvider(UserSettingsManager.UserSettings.LogParserSettings);
-            builtInItems.Add(adpnlog);
-            Items = builtInItems;
-        }
+        public IEnumerable<IAnalogyDataProvider> DataProviders { get; } = new List<IAnalogyDataProvider> { new NLogDataProvider(UserSettingsManager.UserSettings.LogParserSettings) };
     }
 
     public class AnalogyNLogCustomActionFactory : IAnalogyCustomActionsFactory
     {
+        public Guid FactoryId { get; } = NLogBuiltInFactory.AnalogyNLogGuid;
         public string Title { get; } = "Analogy NLog Built-In tools";
-        public IEnumerable<IAnalogyCustomAction> Items { get; }
-
-        public AnalogyNLogCustomActionFactory()
-        {
-            Items = new List<IAnalogyCustomAction>(0);
-        }
+        public IEnumerable<IAnalogyCustomAction> Actions { get; } = new List<IAnalogyCustomAction>(0);
     }
 
     public class AnalogyNLogSettings : IAnalogyDataProviderSettings
