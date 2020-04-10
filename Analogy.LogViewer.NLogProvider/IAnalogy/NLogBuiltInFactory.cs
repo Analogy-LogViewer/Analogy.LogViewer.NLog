9e5b6d2900ea96e1673021c80497dde67ccb0d42
Lior Banai <liorbanai@gmail.com>
2020-04-10 13:02:01 +0300

    fix interface

--- Analogy.LogViewer.NLogProvider/IAnalogy/NLogBuiltInFactory.cs
+++ Analogy.LogViewer.NLogProvider/IAnalogy/NLogBuiltInFactory.cs
@@ -38,7 +38,7 @@ namespace Analogy.LogViewer.NLogProvider
     public class AnalogyNLogSettings : IAnalogyDataProviderSettings
     {
 
-        public Guid ID { get; } = new Guid("8D24EC70-60C0-4823-BE9C-F4A59303FFB3");
+        public Guid ID { get; set; } = new Guid("8D24EC70-60C0-4823-BE9C-F4A59303FFB3");
         public Guid FactoryId { get; set; } = NLogBuiltInFactory.AnalogyNLogGuid;
         public string Title { get; } = "NLog Settings";
         public UserControl DataProviderSettings { get; } = new NLogSettings();
