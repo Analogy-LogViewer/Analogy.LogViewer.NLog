1966792a8dcbbcb84efa7d10bb9809734c6f1e9b
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-25 22:25:03 +0200

    add download information

--- Analogy.LogViewer.NLogProvider/IAnalogy/PrimaryFactory.cs
+++ Analogy.LogViewer.NLogProvider/IAnalogy/PrimaryFactory.cs
@@ -10,15 +10,15 @@ using System.Windows.Forms;
 namespace Analogy.LogViewer.NLogProvider
 {
 
-    public class NLogBuiltInFactory : IAnalogyFactory
+    public class PrimaryFactory : IAnalogyFactory
     {
-        public static Guid AnalogyNLogGuid { get; } = new Guid("33CBFA00-DA3E-4F9F-B5A1-BE978FD09D57");
+        public static Guid Id { get; } = new Guid("33CBFA00-DA3E-4F9F-B5A1-BE978FD09D57");
         public void RegisterNotificationCallback(INotificationReporter notificationReporter)
         {
             
         }
 
-        public Guid FactoryId { get; set; } = AnalogyNLogGuid;
+        public Guid FactoryId { get; set; } = Id;
         public string Title { get; set; } = "NLogs Parser";
         public IEnumerable<IAnalogyChangeLog> ChangeLog { get; set; } = NLogProvider.ChangeLog.GetChangeLog();
         public IEnumerable<string> Contributors { get; set; } = new List<string> { "Lior Banai" };
@@ -31,14 +31,14 @@ namespace Analogy.LogViewer.NLogProvider
 
     public class AnalogyNLogDataProviderFactory : IAnalogyDataProvidersFactory
     {
-        public Guid FactoryId { get; set; } = NLogBuiltInFactory.AnalogyNLogGuid;
+        public Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public string Title { get; set; } = "Analogy NLogs Data Provider";
         public IEnumerable<IAnalogyDataProvider> DataProviders { get; } = new List<IAnalogyDataProvider> { new NLogDataProvider(UserSettingsManager.UserSettings.LogParserSettings) };
     }
 
     public class AnalogyNLogCustomActionFactory : IAnalogyCustomActionsFactory
     {
-        public Guid FactoryId { get; set; } = NLogBuiltInFactory.AnalogyNLogGuid;
+        public Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public string Title { get; set; } = "Analogy NLog Built-In tools";
         public IEnumerable<IAnalogyCustomAction> Actions { get; } = new List<IAnalogyCustomAction>(0);
     }
@@ -47,7 +47,7 @@ namespace Analogy.LogViewer.NLogProvider
     {
 
         public Guid Id { get; set; } = new Guid("8D24EC70-60C0-4823-BE9C-F4A59303FFB3");
-        public Guid FactoryId { get; set; } = NLogBuiltInFactory.AnalogyNLogGuid;
+        public Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public string Title { get; set; } = "NLog Settings";
         public UserControl DataProviderSettings { get; } = new NLogSettings();
         public Image SmallImage { get; set; } = Resources.nlog;
